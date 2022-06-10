f046848ae74ae0717f0a128ceba797cb3243beae
Henry Wilde <henrydavidwilde@gmail.com>
2022-06-10 10:38:35 +0100

    Format tests with `black`.

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -9,60 +9,68 @@ import synthgauge as sg
 
 @pytest.fixture
 def evaluator():
-    return sg.Evaluator(sg.datasets.make_blood_types_df(0, 0, 314),
-                        sg.datasets.make_blood_types_df(1, 0, 314))
+    return sg.Evaluator(
+        sg.datasets.make_blood_types_df(0, 0, 314),
+        sg.datasets.make_blood_types_df(1, 0, 314),
+    )
 
 
 def test_add_metric(evaluator):
     """Check basic add_metric functionality adds metric to metric
     dictionary.
     """
-    evaluator.add_metric('correlation_ratio_MSE')
-    assert evaluator.metrics == {'correlation_ratio_MSE': {}}
+    evaluator.add_metric("correlation_ratio_MSE")
+    assert evaluator.metrics == {"correlation_ratio_MSE": {}}
 
 
 def test_add_metric_alias(evaluator):
     """Check add_metric functionality with alias."""
-    evaluator.add_metric('correlation_ratio_MSE', metric_alias='cor')
+    evaluator.add_metric("correlation_ratio_MSE", metric_alias="cor")
     assert evaluator.metrics == {
-        'cor': {'metric_name': 'correlation_ratio_MSE'}}
+        "cor": {"metric_name": "correlation_ratio_MSE"}
+    }
 
 
 def test_add_metric_arguments(evaluator):
     """Check add_metric functionality with arguments."""
-    evaluator.add_metric('wasserstein', feature='age')
-    assert evaluator.metrics == {'wasserstein': {'feature': 'age'}}
+    evaluator.add_metric("wasserstein", feature="age")
+    assert evaluator.metrics == {"wasserstein": {"feature": "age"}}
 
 
 def test_add_metric_not_implemented(evaluator):
     """Check adding unknown metric returns error."""
     with pytest.raises(NotImplementedError):
-        evaluator.add_metric('custom')
+        evaluator.add_metric("custom")
 
 
 def test_custom_metric_def(evaluator):
     """Check adding custom metric with defined function."""
+
     def length_diff(real, synth):
-        return abs(len(real)-len(synth))
-    evaluator.add_custom_metric('length_diff', length_diff)
-    assert evaluator.evaluate() == {'length_diff': 0}
+        return abs(len(real) - len(synth))
+
+    evaluator.add_custom_metric("length_diff", length_diff)
+    assert evaluator.evaluate() == {"length_diff": 0}
 
 
 def test_custom_metric_args(evaluator):
     """Check adding custom metric with arguments."""
+
     def mean_diff(real, synth, feat):
         return abs(mean(real[feat]) - mean(synth[feat]))
-    evaluator.add_custom_metric('mean_diff', mean_diff, feat='weight')
-    assert evaluator.evaluate() == {'mean_diff': 0.19900000000001228}
+
+    evaluator.add_custom_metric("mean_diff", mean_diff, feat="weight")
+    assert evaluator.evaluate() == {"mean_diff": 0.19900000000001228}
 
 
 def test_copy_metrics(evaluator):
     """Check copying metrics from one Evaluator to another."""
     evaluator_2 = deepcopy(evaluator)
-    evaluator.add_metric('wasserstein', feature='age')
-    evaluator.add_metric('correlation_ratio_MSE', metric_alias='cor')
-    evaluator.add_custom_metric('len_diff',
-                                lambda real, synth: abs(len(real)-len(synth)))
+    evaluator.add_metric("wasserstein", feature="age")
+    evaluator.add_metric("correlation_ratio_MSE", metric_alias="cor")
+    evaluator.add_custom_metric(
+        "len_diff", lambda real, synth: abs(len(real) - len(synth))
+    )
     evaluator_2.copy_metrics(evaluator)
     assert evaluator.metrics == evaluator_2.metrics
 
@@ -76,6 +84,6 @@ def test_copy_metrics_empty(evaluator):
 
 def test_drop_metric(evaluator):
     """Check dropping metric functionality."""
-    evaluator.add_metric('correlation_ratio_MSE')
-    evaluator.drop_metric('correlation_ratio_MSE')
+    evaluator.add_metric("correlation_ratio_MSE")
+    evaluator.drop_metric("correlation_ratio_MSE")
     assert evaluator.metrics == {}
--- tests/test_plot.py
+++ tests/test_plot.py
@@ -3,21 +3,24 @@ import pytest
 from seaborn.axisgrid import JointGrid
 
 from synthgauge.datasets import make_blood_types_df
-from synthgauge.plot import (plot_correlation, plot_histograms, plot_joint,
-                             plot_histogram3d, plot_crosstab)
+from synthgauge.plot import (
+    plot_correlation,
+    plot_histograms,
+    plot_joint,
+    plot_histogram3d,
+    plot_crosstab,
+)
 
 
 @pytest.fixture
 def make_real():
-    """Real data.
-    """
+    """Real data."""
     return make_blood_types_df(0, 0)
 
 
-@ pytest.fixture
+@pytest.fixture
 def make_synth():
-    """Synthetic data that differs from the real data.
-    """
+    """Synthetic data that differs from the real data."""
     return make_blood_types_df(1, 0)
 
 
@@ -30,13 +33,12 @@ def test_plot_hist(make_real):
     assert len(fig.axes) == len(make_real.columns)
 
 
-@pytest.mark.parametrize('x, y', [('age', 'height'),
-                                  ('age', 'eye_colour'),
-                                  ('eye_colour', 'hair_colour')]
-                         )
+@pytest.mark.parametrize(
+    "x, y",
+    [("age", "height"), ("age", "eye_colour"), ("eye_colour", "hair_colour")],
+)
 def test_plot_joint(make_real, x, y):
-    """Check correct figure type returned with expected axes
-    """
+    """Check correct figure type returned with expected axes"""
     fig = plot_joint(make_real, x, y)
 
     # Check figure type
@@ -47,13 +49,12 @@ def test_plot_joint(make_real, x, y):
     assert fig.ax_joint.get_ylabel() == y
 
 
-@pytest.mark.parametrize('x, y', [('age', 'height'),
-                                  ('age', 'eye_colour'),
-                                  ('eye_colour', 'hair_colour')]
-                         )
+@pytest.mark.parametrize(
+    "x, y",
+    [("age", "height"), ("age", "eye_colour"), ("eye_colour", "hair_colour")],
+)
 def test_plot_hist3d_numeric(make_real, x, y):
-    """Check correct figure type returned with expected axes
-    """
+    """Check correct figure type returned with expected axes"""
     fig = plot_histogram3d(make_real, x, y)
 
     # Check figure type
@@ -64,17 +65,20 @@ def test_plot_hist3d_numeric(make_real, x, y):
     assert fig.axes[0].get_ylabel() == y
 
 
-@pytest.mark.parametrize('feats, method, plot_diff',
-                         [(None, 'pearson', False),
-                          (None, 'pearson', True),
-                          (['age', 'height'], 'spearman', False),
-                          (['eye_colour', 'hair_colour'], 'cramers_v', False)]
-                         )
+@pytest.mark.parametrize(
+    "feats, method, plot_diff",
+    [
+        (None, "pearson", False),
+        (None, "pearson", True),
+        (["age", "height"], "spearman", False),
+        (["eye_colour", "hair_colour"], "cramers_v", False),
+    ],
+)
 def test_plot_correlation(make_real, make_synth, feats, method, plot_diff):
-    """Check correct figure type returned with correct number of axes.
-    """
-    fig = plot_correlation(make_real, make_synth, feats=feats, method=method,
-                           plot_diff=plot_diff)
+    """Check correct figure type returned with correct number of axes."""
+    fig = plot_correlation(
+        make_real, make_synth, feats=feats, method=method, plot_diff=plot_diff
+    )
 
     assert isinstance(fig, Figure)
     assert len(fig.axes) == 4 if not plot_diff else 6
@@ -90,40 +94,47 @@ def test_plot_correlation(make_real, make_synth, feats, method, plot_diff):
     # Check correct features used
     if feats is None:
         feats = make_real.columns
-    if method in ['pearson', 'spearman']:
-        expected_feats = make_real[feats].select_dtypes(include='number') \
-            .columns
-    elif method == 'cramers_v':
-        expected_feats = make_real[feats].select_dtypes(exclude='number') \
-            .columns
+    if method in ["pearson", "spearman"]:
+        expected_feats = (
+            make_real[feats].select_dtypes(include="number").columns
+        )
+    elif method == "cramers_v":
+        expected_feats = (
+            make_real[feats].select_dtypes(exclude="number").columns
+        )
     assert expected_feats.to_list() == x_labels
 
 
-@pytest.mark.parametrize('feats, method,',
-                         [(['age', 'height'], 'cramers_v'),
-                          (['eye_colour', 'hair_colour'], 'pearson')]
-                         )
+@pytest.mark.parametrize(
+    "feats, method,",
+    [
+        (["age", "height"], "cramers_v"),
+        (["eye_colour", "hair_colour"], "pearson"),
+    ],
+)
 def test_plot_correlation_method(make_real, make_synth, feats, method):
     with pytest.raises(ValueError):
         plot_correlation(make_real, make_synth, feats=feats, method=method)
 
 
-@pytest.mark.parametrize('x, y, x_bins, y_bins',
-                         [('age', 'height', 'auto', 'auto'),
-                          ('eye_colour', 'hair_colour', 5, 5),
-                          ('height', 'eye_colour', 10, 'auto')]
-                         )
+@pytest.mark.parametrize(
+    "x, y, x_bins, y_bins",
+    [
+        ("age", "height", "auto", "auto"),
+        ("eye_colour", "hair_colour", 5, 5),
+        ("height", "eye_colour", 10, "auto"),
+    ],
+)
 def test_plot_crosstab(make_real, make_synth, x, y, x_bins, y_bins):
-    """
-    """
+    """ """
     fig = plot_crosstab(make_real, make_synth, x, y, x_bins, y_bins)
     assert isinstance(fig, Figure)
 
 
-@pytest.mark.parametrize('x, y, x_bins, y_bins',
-                         [('age', 'height', None, None),
-                          ('height', 'eye_colour', None, 'auto')]
-                         )
+@pytest.mark.parametrize(
+    "x, y, x_bins, y_bins",
+    [("age", "height", None, None), ("height", "eye_colour", None, "auto")],
+)
 def test_plot_crosstab_bins(make_real, make_synth, x, y, x_bins, y_bins):
     with pytest.raises(TypeError):
         plot_crosstab(make_real, make_synth, x, y, x_bins, y_bins)
