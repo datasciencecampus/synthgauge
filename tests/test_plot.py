960efb66ea7eb2c5755cbb77b2fa2a81f982bafc
Henry Wilde <henrydavidwilde@gmail.com>
2022-06-17 12:34:48 +0100

    Write tests for `plot_crosstab()`
    Three tests for this function:
    
    - a generic test checking label names and total counts, etc
    - a test for the (redundant?) `TypeError`
    - a test ensuring the bug from #1 doesn't come back around

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -7,14 +7,8 @@ from hypothesis import HealthCheck, given, settings
 from hypothesis import strategies as st
 from seaborn.axisgrid import JointGrid
 
+from synthgauge import plot
 from synthgauge.datasets import make_blood_types_df
-from synthgauge.plot import (
-    plot_correlation,
-    plot_histogram3d,
-    plot_histograms,
-    plot_joint,
-    suggest_label_rotation,
-)
 
 from .utils import resolve_features
 
@@ -45,7 +39,7 @@ plot_settings = settings(
 @st.composite
 def joint_params(draw):
     """Get a valid set of joint-plot parameters. Recycled by the 3D
-    histogram test."""
+    histogram and crosstab plot tests."""
 
     x, y = draw(
         st.lists(
@@ -89,7 +83,7 @@ def test_suggest_label_rotation(labels, axis, limit):
     getattr(ax, f"set_{axis}ticks")(range(len(labels)))
     getattr(ax, f"set_{axis}ticklabels")(labels)
 
-    rotate = suggest_label_rotation(ax, axis, limit)
+    rotate = plot.suggest_label_rotation(ax, axis, limit)
     assert rotate is any(len(label) > limit for label in labels)
 
 
@@ -102,7 +96,7 @@ def test_plot_histograms(real, feats):
     columns = resolve_features(feats, real)
     num_columns = len(columns)
 
-    fig = plot_histograms(real, feats)
+    fig = plot.plot_histograms(real, feats)
     axes = fig.axes
     num_axes = len(axes)
 
@@ -137,7 +131,7 @@ def test_plot_joint(real, params):
 
     x, y, groupby = params
 
-    fig = plot_joint(real, x, y, groupby=groupby)
+    fig = plot.plot_joint(real, x, y, groupby=groupby)
     ax, axx, axy = fig.ax_joint, fig.ax_marg_x, fig.ax_marg_y
 
     assert isinstance(fig, JointGrid)
@@ -178,7 +172,7 @@ def test_plot_histogram3d(real, params):
 
     x, y, _ = params
 
-    fig = plot_histogram3d(real, x, y)
+    fig = plot.plot_histogram3d(real, x, y)
     ax = fig.axes[0]
 
     assert isinstance(fig, plt.Figure)
@@ -208,7 +202,7 @@ def test_plot_correlation_methods(real, method):
     """Check that a correlation heatmap can be created for a single
     dataset with any of the implemented methods."""
 
-    fig = plot_correlation(real, method=method)
+    fig = plot.plot_correlation(real, method=method)
     ax, cbar = fig.axes
 
     assert isinstance(fig, plt.Figure)
@@ -243,7 +237,7 @@ def test_plot_correlation_method_errors(real, method):
         column, match = "blood_type", "^No numeric columns"
 
     with pytest.raises(ValueError, match=match):
-        _ = plot_correlation(real, feats=column, method=method)
+        _ = plot.plot_correlation(real, feats=column, method=method)
 
 
 @given(method=st.sampled_from(("pearson", "spearman", "cramers_v")))
@@ -252,7 +246,7 @@ def test_plot_correlation_two_datasets(real, synth, method):
     """Check that an array of correlation heatmaps can be produced when
     two dataframes are passed."""
 
-    fig = plot_correlation(real, synth, method=method)
+    fig = plot.plot_correlation(real, synth, method=method)
     rax, sax, rbar, sbar = fig.axes
 
     assert isinstance(fig, plt.Figure)
@@ -278,7 +272,7 @@ def test_plot_correlation_difference(real, synth, method):
     """Check that an additional heatmap can be produced for the
     absolute difference between two correlation matrices."""
 
-    fig = plot_correlation(real, synth, plot_diff=True, method=method)
+    fig = plot.plot_correlation(real, synth, plot_diff=True, method=method)
     rax, sax, dax, empty, rbar, sbar, dbar = fig.axes
 
     assert isinstance(fig, plt.Figure)
@@ -306,3 +300,72 @@ def test_plot_correlation_difference(real, synth, method):
             columns, ax.get_xticklabels(), ax.get_yticklabels()
         ):
             assert {col, xlab.get_text(), ylab.get_text()} == {col}
+
+
+@given(params=joint_params())
+@plot_settings
+def test_plot_crosstab(real, synth, params):
+    """Check that an array of heatmaps can be produced correctly."""
+
+    x, y, _ = params
+    fig = plot.plot_crosstab(real, synth, x, y)
+    rax, sax, cbar = fig.axes
+
+    assert isinstance(fig, plt.Figure)
+    assert isinstance(cbar, plt.Axes)
+    assert rax.get_title() == "REAL" and sax.get_title() == "SYNTH"
+    assert cbar.get_label() == "<colorbar>"
+
+    for ax, data in zip((rax, sax), (real, synth)):
+        assert isinstance(ax, plt.Subplot)
+        assert ax.get_xlabel() == x
+        assert ax.get_ylabel() == y
+
+        total_count = sum(ax.collections[0]._A)
+        binning_error = sum(
+            pd.api.types.is_numeric_dtype(data[col]) for col in (x, y)
+        )
+        nrows = len(data)
+        assert total_count in range(nrows - binning_error, nrows + 1)
+
+
+@given(
+    columns=st.lists(
+        st.sampled_from(("age", "height", "weight")),
+        unique=True,
+        min_size=2,
+        max_size=2,
+    )
+)
+@plot_settings
+def test_plot_crosstab_error(real, synth, columns):
+    """Check that a TypeError is thrown when neither binning method is
+    specified for a crosstab plot of two numeric columns."""
+
+    with pytest.raises(
+        TypeError, match="^`x_bins` and `y_bins` must not be None$"
+    ):
+        x, y = columns
+        _ = plot.plot_crosstab(real, synth, x, y, x_bins=None, y_bins=None)
+
+
+@given(
+    params=joint_params(),
+    cmap=st.sampled_from(("viridis", "ch:s=.25,rot=-.25", "light:coral")),
+)
+@plot_settings
+def test_plot_crosstab_colour_palette(real, synth, params, cmap):
+    """Check that a colour palette (map) can be passed to a crosstab
+    plot correctly, following through to each heatmap and the colour
+    bar.
+
+    This test serves to cover this issue (#1) in the repository:
+        https://github.com/datasciencecampus/synthgauge/issues/1
+    """
+
+    x, y, _ = params
+    fig = plot.plot_crosstab(real, synth, x, y, cmap=cmap)
+    rax, sax, cbar = fig.axes
+
+    assert rax.collections[0].cmap is sax.collections[0].cmap
+    assert rax.collections[0].cmap is cbar.collections[-1].cmap
