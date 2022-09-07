25eb1644b67a538124d2218da5d43167dc907eb7
Henry Wilde <henrydavidwilde@gmail.com>
2022-09-07 17:08:00 +0100

    Write body of first NIST measure.

--- src/synthgauge/metrics/nist.py
+++ src/synthgauge/metrics/nist.py
@@ -1,13 +1,98 @@
 """Functions for the generic measures from the 2018 NIST competition."""
 
+import itertools
 
-def three_way_marginals(real, synth, seed=None):
-    """The first generic measure based on similarity of 3-way marginals.
+import numpy as np
+import pandas as pd
 
-    In essence, calculate the summed absolute deviation between the real
-    and synthetic data across an array of randomly sampled 3-way
-    marginals. Transform and summarise these deviations to give a single
-    score between 0 and 1.
 
-    Details can be found at https://doi.org/10.6028/NIST.TN.2151.
+def _numeric_edges(real, synth, bins):
+    """Find the bin edges for the numeric features."""
+
+    numeric = real.select_dtypes(include="number").columns.intersection(
+        synth.columns
+    )
+    edges = {col: np.histogram_bin_edges(real[col], bins) for col in numeric}
+
+    return edges
+
+
+def _discretise_datasets(real, synth, bins):
+    """Discretise the numeric features of each dataset."""
+
+    rout, sout = real.copy(), synth.copy()
+    edges = _numeric_edges(rout, sout, bins)
+    for col, edge in edges.items():
+        rout.loc[:, col] = pd.cut(rout[col], edge)
+        sout.loc[:, col] = pd.cut(sout[col], edge)
+
+    return rout, sout
+
+
+def _kway_marginal_score(real, synth, features):
+    """Get the transformed score for a single set of features.
+
+    Note that the datasets should have their numeric features
+    discretised already.
     """
+
+    rmarg = real.groupby(features).size() / len(real)
+    smarg = synth.groupby(features).size() / len(synth)
+
+    return 1 - sum(abs(rmarg - smarg)) / 2
+
+
+def kway_marginals(real, synth, k=3, trials=100, bins=100, seed=None):
+    """The first generic measure based on similarity of k-way marginals.
+
+    In essence, calculate the summed absolute difference in density
+    across an array of randomly sampled k-way marginals. Transform and
+    summarise these deviations to give a single score between 0 and 1.
+    These extremes represent the worst and best case scenarios,
+    respectively.
+
+    The NIST competition utilised 3-way marginals only. Details can be
+    found at https://doi.org/10.6028/NIST.TN.2151.
+
+    Parameters
+    ----------
+    real : pandas.DataFrame
+        Dataframe containing the real data.
+    synth : pandas.DataFrame
+        Dataframe containing the synthetic data.
+    k : int, default 3
+        Number of features to include in each k-way marginal. Default
+        uses 3 (as done in the NIST competition).
+    trials : int, default 100
+        Maximum number of marginals to consider to estimate the overall
+        score. If there are fewer `k`-way combinations than `trials`,
+        tries all. Default uses 100 (as done in the NIST competition).
+    bins : int or str, default 100
+        Binning method for sampled numeric features. Can be anything
+        accepted by `numpy.histogram_bin_edges`. Default uses 100 bins
+        (as done in the NIST competition).
+    seed : int or None, default None
+        Random number seed. If `None`, results will not be reproducible.
+
+    Returns
+    -------
+    score : float
+        The mean transformed summed absolute deviation in k-way densities.
+    """
+
+    disreal, dissynth = _discretise_datasets(real, synth, bins)
+    prng = np.random.default_rng(seed)
+
+    choices = list(
+        itertools.combinations(real.columns.intersection(synth.columns), r=k)
+    )
+    marginals = prng.choice(
+        choices, size=min(trials, len(choices)), replace=False
+    ).tolist()
+
+    scores = [
+        _kway_marginal_score(disreal, dissynth, marginal)
+        for marginal in marginals
+    ]
+
+    return np.mean(scores)
--- tests/test_metrics_nist.py
+++ tests/test_metrics_nist.py
@@ -1,5 +1,7 @@
 """Tests for the NIST 2018 metrics."""
 
+import pandas as pd
+import numpy as np
 from hypothesis import assume, given
 from hypothesis import strategies as st
 
@@ -8,14 +10,98 @@ from synthgauge.metrics import nist
 from .utils import datasets
 
 
-@given(datasets(), st.integers(0, 100))
-def test_three_way_marginals(datasets, seed):
-    """Test that the 3-way marginal score can be calculated."""
+@given(
+    datasets(
+        column_spec={"a": "float", "b": "float", "c": "object"},
+        min_value=0,
+        max_value=100,
+        allow_nan=False,
+    ),
+    st.sampled_from(["auto", 10, 100]),
+)
+def test_numeric_edges(datasets, bins):
+    """Test the numeric feature edges can be found correctly."""
 
     real, synth = datasets
     assume(not (real.empty or synth.empty))
 
-    score = nist.three_way_marginals(real, synth, seed)
+    edges = nist._numeric_edges(real, synth, bins)
+
+    assert isinstance(edges, dict)
+    assert list(edges.keys()) == ["a", "b"]
+
+    for col, edge in edges.items():
+        assert isinstance(edge, np.ndarray)
+        assert edge.ndim == 1
+        assert min(edge) <= real[col].min()
+        assert max(edge) >= real[col].max()
+        if isinstance(bins, int):
+            assert edge.size == bins + 1
+
+
+@given(
+    datasets(
+        column_spec={"a": "float", "b": "float", "c": "object"},
+        min_value=0,
+        max_value=100,
+        allow_nan=False,
+    ),
+    st.sampled_from(["auto", 10, 100]),
+)
+def test_discretise_datasets(datasets, bins):
+    """Test that a real-synthetic pair can be discretised."""
+
+    real, synth = datasets
+    assume(not (real.empty or synth.empty))
+
+    disreal, dissynth = nist._discretise_datasets(real, synth, bins)
+
+    numeric = ["a", "b"]
+    for col in numeric:
+        assert np.array_equal(disreal[col].unique(), dissynth[col].unique())
+
+    for disc, orig in zip((disreal, dissynth), (real, synth)):
+        assert isinstance(disc, pd.DataFrame)
+        assert disc.columns.equals(orig.columns)
+        assert disc.drop(numeric, axis=1).equals(
+            orig.select_dtype(exclude="number")
+        )
+
+
+@given(
+    datasets(available_dtypes=("object",), min_columns=3),
+    st.lists(st.sampled_from(["a", "b", "c"]), unique=True, min_size=1).map(
+        sorted
+    ),
+)
+def test_kway_marginal_score(datasets, features):
+    """Test that the transformed marginal score can be calculated."""
+
+    real, synth = datasets
+    assume(not (real.empty or synth.empty))
+
+    score = nist._kway_marginal_score(real, synth, features)
+
+    assert isinstance(score, float)
+    assert score >= 0 and score <= 1
+
+
+@given(
+    datasets(
+        column_spec={"a": "float", "b": "float", "c": "object"},
+        min_value=0,
+        max_value=100,
+        allow_nan=False,
+    ),
+    st.sampled_from(["auto", 10, 100]),
+)
+def test_kway_marginals(datasets, seed):
+    """Test that the k-way marginal score can be calculated."""
+
+    real, synth = datasets
+    assume(not (real.empty or synth.empty))
+
+    score = nist.kway_marginals(real, synth, seed=seed)
 
     assert isinstance(score, float)
     assert score >= 0 and score <= 1
