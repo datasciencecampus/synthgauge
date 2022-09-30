368f1d5f7f805eeb85b6cfb8479fac747191eb05
Henry Wilde <henrydavidwilde@gmail.com>
2022-09-30 13:10:24 +0100

    Add explicit ipython dependency
    Syntax highlighting only works if ipython is installed

--- src/synthgauge/metrics/__init__.py
+++ src/synthgauge/metrics/__init__.py
@@ -4,6 +4,7 @@ from .classification import classification_comparison
 from .cluster import clustered_msd
 from .correlation import correlation_msd, correlation_ratio_msd
 from .density import feature_density_mad
+from .nist import hoc, kway_marginals
 from .privacy import min_nearest_neighbour, sample_overlap_score, tcap_score
 from .propensity import propensity_metrics
 from .univariate import (
@@ -23,11 +24,13 @@ __all__ = [
     "correlation_msd",
     "correlation_ratio_msd",
     "feature_density_mad",
+    "hoc",
     "jensen_shannon_distance",
     "jensen_shannon_divergence",
     "kolmogorov_smirnov",
     "kruskal_wallis",
     "kullback_leibler",
+    "kway_marginals",
     "mann_whitney",
     "min_nearest_neighbour",
     "propensity_metrics",
